8944015784f50db200f8918647c6805183d7c20f
Michael Zullo <rockhero840@gmail.com>
2023-08-29 13:48:23 -0400

    Complete qualifued assignment, ...

---
+++ public/src/books.js
@@ -0,0 +1,54 @@
+//return the author object when given the id
+function findAuthorById(authors, id) {
+  let authId = authors.find((author) => (author.id === id));
+  return authId;
+}
+//passed
+//======================================================================
+
+//return the book object when given the id
+function findBookById(books, id) {
+  let bookId = books.find((book) => (book.id === id));
+  return bookId;
+}
+//passed
+//======================================================================
+
+// NOTE: YOU DON'T HAVE TO EDIT THE FUNCTIONS BELOW
+function partitionBooksByBorrowedStatus(books) {
+  return books.reduce(
+    (acc, book) => {
+      const [borrowed, returned] = acc;
+      const recent = book.borrows[0];
+      if (recent.returned) {
+        returned.push(book);
+      } else {
+        borrowed.push(book);
+      }
+
+      return acc;
+    },
+    [[], []]
+  );
+}
+
+function getBorrowersForBook(book, accounts) {
+  const accountsById = accounts.reduce((acc, account) => {
+    acc[account.id] = account;
+    return acc;
+  }, {});
+
+  return book.borrows
+    .map(({ id, returned }) => ({
+      ...accountsById[id],
+      returned,
+    }))
+    .slice(0, 10);
+}
+
+module.exports = {
+  findAuthorById,
+  findBookById,
+  partitionBooksByBorrowedStatus,
+  getBorrowersForBook,
+};
